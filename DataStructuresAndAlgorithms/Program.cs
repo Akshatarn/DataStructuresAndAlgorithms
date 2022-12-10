1cbb4515a6f962319cf80b1909381432cfefb481
akshata_rn <akshatarn123@gamail.com>
2022-12-10 22:47:59 +0530

    Binary search program

---
+++ DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    internal class Program
+    {
+        static int binarySearch(String[] arr, String x)
+        {
+            int l = 0, r = arr.Length - 1;
+            while (l <= r)
+            {
+                int m = l + (r - l) / 2;
+
+                int res = x.CompareTo(arr[m]);
+                if (res == 0)
+                    return m;
+                if (res > 0)
+                    l = m + 1;
+                else
+                    r = m - 1;
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            String[] arr = { "Ab", "cd", "ef", "gh" };
+            Console.WriteLine("Enter the word you want to search");
+            String x = Convert.ToString(Console.ReadLine());
+            int result = binarySearch(arr, x);
+            if (result == -1)
+                Console.WriteLine("Element not present");
+            else
+                Console.WriteLine("Element found at "
+                                + "index " + result);
+
+        }
+    }
+}
